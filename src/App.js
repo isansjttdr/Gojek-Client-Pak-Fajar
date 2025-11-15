dd337e545be21ed4610475abaed9aa5d1ba5e5ae
isansjttdr <145756410+isansjttdr@users.noreply.github.com>
2025-11-15 20:12:12 +0700

    Delete src directory

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import GojekMini from "./GojekMini";
-
-function App() {
-  return <GojekMini />;
-}
-
-export default App;
--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = "https://lhdpyvrihbrgrfdqakie.supabase.co";
-const supabaseAnonKey = "sb_publishable_KSagnZojo8lkD1bZIKSAQQ_tYYRlerw";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
